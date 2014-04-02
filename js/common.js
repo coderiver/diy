743b91b508b32b193814af94f789c344444705f5
akella <akella.a@gmail.com>
2014-04-02 11:03:50 +0300

    preview

--- js/common.js
+++ js/common.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
+	// clicking nav
+	$('.nav a').click(function(event) {
+		gotoslide = $(this).data('slide');
+		goto(gotoslide);
+		return false;
+	});
+	function goto(n){
+		slide = $('#slide'+n);
+		  $('html, body').animate({
+	        scrollTop: slide.offset().top
+	    }, 500);
+		$('.nav a').removeClass('is-active');
+		$('.nav a:nth-child('+gotoslide+')').addClass('is-active');
+	}
 
 });
