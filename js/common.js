ae316f534630e707afd7b3f2402579345e4a22ad
akella <akella.a@gmail.com>
2014-04-02 11:14:45 +0300

    fix

--- js/common.js
+++ js/common.js
@@ -2,16 +2,22 @@ $(document).ready(function() {
 	// clicking nav
 	$('.nav a').click(function(event) {
 		gotoslide = $(this).data('slide');
-		goto(gotoslide);
+		goto(gotoslide,500);
 		return false;
 	});
-	function goto(n){
+	function goto(n, time){
 		slide = $('#slide'+n);
 		  $('html, body').animate({
 	        scrollTop: slide.offset().top
-	    }, 500);
+	    }, time);
 		$('.nav a').removeClass('is-active');
 		$('.nav a:nth-child('+gotoslide+')').addClass('is-active');
 	}
+
+	// on rezize stick to slide
+	$(window).resize(function(event) {
+		gotoslide = $('nav .is-active').data('slide');
+		goto(gotoslide,100);
+	});
 
 });
